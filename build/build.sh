70a65d416a9e5df03d4df328379f283c55eeada0
blckmn <blackman@xtra.com.au>
2018-07-15 12:42:22 +1000

    Cleaned up build scripts with braces.

--- build/build.sh
+++ build/build.sh
@@ -2,24 +2,24 @@
 
 set -e
 
-basepath="$PWD"
-artifacts="$basepath/artifacts"
+basepath="${PWD}"
+artifacts="${basepath}/artifacts"
 revision=${TRAVIS_BRANCH:="alpha"}
 major="1"
 minor="0"
 buildnumber=${TRAVIS_BUILD_NUMBER:=1}
 version="${major}.${minor}.${buildnumber}"
 
-if [ "$revision" != "release" ]; then
+if [ "${revision}" != "release" ]; then
   version="${version}-${revision}"
 fi
 export VERSION=${version}
 
-echo "Base path: $basepath"
-echo "Artifacts: $artifacts"
-echo "Revision:  $revision"
-echo "Build #:   $buildnumber"
-echo "Version:   $version"
+echo "Base path: ${basepath}"
+echo "Artifacts: ${artifacts}"
+echo "Revision:  ${revision}"
+echo "Build #:   ${buildnumber}"
+echo "Version:   ${version}"
 
 dotnet restore
 
--- build/deploy.sh
+++ build/deploy.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-basepath="$PWD"
-artifacts="$basepath/artifacts"
+basepath="${PWD}"
+artifacts="${basepath}/artifacts"
 nuget_server="https://www.nuget.org/api/v2/package"
 revision=${TRAVIS_BRANCH:="alpha"}
 major="1"
@@ -11,27 +11,25 @@ minor="0"
 buildnumber=${TRAVIS_BUILD_NUMBER:=1}
 version="${major}.${minor}.${buildnumber}"
 
-if [ "$revision" != "release" ]; then
+if [ "${revision}" != "release" ]; then
   version="${version}-${revision}"
 fi
 export VERSION=${version}
 
-if [ -d $artifacts ]; then  
-  rm -R $artifacts
+if [ -d ${artifacts} ]; then  
+  rm -R ${artifacts}
 fi
 
-echo "Base path: $basepath"
-echo "Artifacts: $artifacts"
-echo "Revision:  $revision"
-echo "Build #:   $buildnumber"
-echo "Version:   $version"
+echo "Base path: ${basepath}"
+echo "Artifacts: ${artifacts}"
+echo "Revision:  ${revision}"
+echo "Build #:   ${buildnumber}"
+echo "Version:   ${version}"
 
-if [ "$revision" == "release" ]; then
-  export BUILD=""
-  dotnet pack -c Release -o $artifacts 
+if [ "${revision}" == "release" ]; then
+  dotnet pack -c Release -o ${artifacts} 
 else
-  export BUILD=${revision}
-  dotnet pack -c Release -o $artifacts --version-suffix=$revision 
+  dotnet pack -c Release -o ${artifacts} --version-suffix=${revision} 
 fi
 
-dotnet nuget push "$artifacts/*.nupkg" -s $nuget_server -k $nuget_access_key
+dotnet nuget push "${artifacts}/*.nupkg" -s ${nuget_server} -k ${NUGET_ACCESS_KEY}
