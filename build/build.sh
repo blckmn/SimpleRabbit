bf8631c8d4dfe3c113cf42423525643fddb093e9
BLCKMN <blackman@xtra.com.au>
2018-07-17 15:19:02 +1000

    Moved common elements as part of build and deploy scripts.

--- build/build.sh
+++ build/build.sh
@@ -2,24 +2,7 @@
 
 set -e
 
-basepath="${PWD}"
-artifacts="${basepath}/artifacts"
-revision=${TRAVIS_BRANCH:="alpha"}
-major="1"
-minor="0"
-buildnumber=${TRAVIS_BUILD_NUMBER:=1}
-version="${major}.${minor}.${buildnumber}"
-
-if [ "${revision}" != "release" ]; then
-  version="${version}-${revision}"
-fi
-export VERSION=${version}
-
-echo "Base path: ${basepath}"
-echo "Artifacts: ${artifacts}"
-echo "Revision:  ${revision}"
-echo "Build #:   ${buildnumber}"
-echo "Version:   ${version}"
+source "${PWD}/build/common.sh"
 
 dotnet restore
 
