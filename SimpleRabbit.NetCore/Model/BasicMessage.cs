a6f8ad3886fcac92562caed2b6dfcb05eee98bc8
blckmn <blackman@xtra.com.au>
2018-08-08 17:11:20 +1000

    Added helper methods for Ack and Nack, and relocated to alternate file.

---
+++ SimpleRabbit.NetCore/Model/BasicMessage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace SimpleRabbit.NetCore
+{
+    public class BasicMessage
+    {
+        public BasicMessage(BasicDeliverEventArgs deliveryArgs, IModel channel, string queue, Action registerError)
+        {
+            DeliveryArgs = deliveryArgs;
+            Channel = channel;
+            Queue = queue;
+            RegisterError = registerError;
+        }
+
+        public BasicDeliverEventArgs DeliveryArgs { get; }
+        public IModel Channel { get; }
+        public string Queue { get; }
+        public Action RegisterError { get; }
+
+        public string Body => Encoding.UTF8.GetString(DeliveryArgs?.Body);
+        public IBasicProperties Properties => DeliveryArgs?.BasicProperties;
+        public ulong DeliveryTag => DeliveryArgs?.DeliveryTag ?? 0;
+        public string ConsumerTag => DeliveryArgs?.ConsumerTag;
+        public string MessageId => Properties?.MessageId;
+        public IDictionary<string, object> Headers => Properties?.Headers;
+
+        public void Ack()
+        {
+            Channel?.BasicAck(DeliveryTag, false);
+        }
+
+        public void Nack(bool requeue = true)
+        {
+            Channel?.BasicNack(DeliveryTag, false, requeue);
+        }
+    }
+}
