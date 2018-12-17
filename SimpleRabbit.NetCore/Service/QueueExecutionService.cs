b63f8adcc3cebafeac3e443858de46bd2cf1db90
blckmn <blackman@xtra.com.au>
2018-12-17 17:24:41 +1100

    Adding dispatching as automatic within main project based on Prefetch setting.

---
+++ SimpleRabbit.NetCore/Service/QueueExecutionService.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleRabbit.NetCore
+{
+    internal class QueueExecutionService
+    {
+        private readonly object _semaphore;
+
+        public QueueExecutionService(object semaphore)
+        {
+            _semaphore = semaphore;
+        }
+
+        private readonly ManualResetEvent _mre = new ManualResetEvent(false);
+
+        private readonly List<QueuedMessage> _queue = new List<QueuedMessage>();
+        private Task Task { get; set; }
+        public void Enqueue(QueuedMessage message)
+        {
+            lock (_semaphore)
+            {
+                _queue.Add(message);
+            }
+
+            _mre.Set();
+        }
+
+        public bool CanEnqueue(string key)
+        {
+            switch (key)
+            {
+                case null:
+                    lock (_semaphore)
+                    {
+                        return _queue.Count == 0;
+                    }
+                default:
+                    lock (_semaphore)
+                    {
+                        return _queue.Any(m => m.Key?.Equals(key, StringComparison.CurrentCultureIgnoreCase) ?? false);
+                    }
+            }
+        }
+
+        private void Process()
+        {
+            while (!_stopped)
+            {
+                QueuedMessage message;
+                lock (_semaphore)
+                {
+                    message = _queue.FirstOrDefault();
+                }
+
+                if (message == null)
+                {
+                    _mre.Reset();
+                    _mre.WaitOne();
+                    continue;
+                }
+
+                try
+                {
+                    if (message.Action?.Invoke(message.Message) ?? false)
+                    {
+                        message.Message.Channel?.BasicAck(message.Message.DeliveryTag, false);
+                    }
+
+                    lock (_semaphore)
+                    {
+                        _queue.Remove(message);
+                    }
+                }
+                catch
+                {
+                    message.Message.RegisterError?.Invoke();
+                    lock (_semaphore)
+                    {
+                        _queue.Clear();
+                    }
+                }
+            }
+        }
+
+        public void Pause()
+        {
+            _mre.Reset();
+        }
+
+        private bool _stopped = true;
+        public void Start()
+        {
+            if (Task == null)
+            {
+                Task = Task.Run(()=>Process());
+            }
+
+            _stopped = false;
+            _mre.Reset();
+        }
+
+        public void Stop()
+        {
+            _stopped = true;
+            _mre.Set();
+            Task?.Wait();
+            Task = null;
+        }
+    }
+}
